5ee05a972ffd4bd0de0181faf577eecea2349c4b
Tan Theng Lee <thengleetan@gmail.com>
2015-04-12 17:41:22 +0800

    Create new users in db

---
+++ app/controllers/users.server.controller.js
@@ -0,0 +1,14 @@
+var User = require('mongoose').model('User');
+
+exports.create = function(req, res, nexr){
+	var user = new User(req.body);
+
+	user.save(function(err){
+		if (err){
+			return next(err);
+		}else{
+			res.json(user);
+		}
+	});
+};
+
