8c0e7b602e42616a00f4ba355dc24207bf8e03c8
Tan Theng Lee <thengleetan@gmail.com>
2015-04-12 22:48:25 +0800

    Update existing user

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -39,3 +39,15 @@ exports.userByID = function(req, res, next, id){
 	});
 };
 
+
+exports.update = function(req, res, next){
+	User.findByIdAndUpdate(req.user.id, req.body, function(err, user){
+		if (err){
+			return next(err);
+		}else{
+			res.json(user);
+		}
+	});
+};
+
+
