6d20228c926eb6af7d83820e22683b6390372635
Tan Theng Lee <thengleetan@gmail.com>
2015-04-12 11:46:25 +0800

    Configure Express application

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express');
+
 var app = express();
 
-app.use('/', function(req, res){
-	res.send('Hello World');
-});
-
 app.listen(3000);
-console.log('Server running at http://localhost:3000/');
 
 module.exports = app;
+
+console.log('Server running at http://localhost:3000/');
